c3a2dc93244ae877bb717a5074e655e5426785b7
Mephisto Lin <Mephisto007linlin@outlook.com>
2024-02-14 22:20:39 +0800

    feat: update opencv start test.

---
+++ OpenCV/OpenCVTest/OpenCVTest/OpenCVTest.cpp
@@ -0,0 +1,26 @@
+﻿#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+
+int main()
+{
+
+    Mat img = imread("..//cutCat.jpg");
+
+
+    if (img.empty())
+    {
+        std::cout << "Could not read the image!" << std::endl;
+        return 1;
+    }
+
+    namedWindow("Display Test", 0);
+    resizeWindow("Display Test", 937, 1250);
+    imshow("Display Test", img);
+
+    int iKey = waitKey(0);
+
+    return 0;
+}
+
